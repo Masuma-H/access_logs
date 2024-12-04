43c59eb40689de697a9f894ca988689fd163d0ae
Student <student@Admins-MacBook-Pro.local>
2024-12-04 16:54:52 -0500

    firsy commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Project: [system check]
+Author: [Masuma Begum]
+Date: [12/03/24]
+
+Interactive data analysis program for [privacy analyzer].
+"""
+
+# 2. Imports
+import pandas as pd
+import matplotlib.pyplot as plt
